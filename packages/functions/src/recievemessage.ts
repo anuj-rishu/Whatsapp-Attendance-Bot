d538fd0a9fca507b00e927ad54d8de30d027e7d7
viralgupta <guptaviral0@gmail.com>
2024-06-01 00:29:00 +0530

    awaited main function

--- packages/functions/src/recievemessage.ts
+++ packages/functions/src/recievemessage.ts
@@ -3,8 +3,7 @@ import Main from "@my-sst-app/core/recievemessage/index"
 
 export const handler = ApiHandler(async(evt) => {
   
-  Main(JSON.parse(evt.body!))
-  console.log(evt.body)
+  await Main(JSON.parse(evt.body!))
   return {
     statusCode: 200,
   };
